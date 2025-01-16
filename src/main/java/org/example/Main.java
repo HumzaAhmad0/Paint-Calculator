cd0f66330c71f71353eb38cdd9d0725651d6aa66
unknown <humzaahmad_@hotmail.com>
2025-01-16 00:28:31 +0000

    Addition of error handling + cleaning up code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,176 +1,175 @@
 package org.example;
 import java.util.Scanner;
-import java.util.Vector;
-
 
 public class Main {
 
-
     public static double surfaceAreaSquare(){
-        Scanner sc = new Scanner(System.in);
-        double tempArea = 0.00;
-        double totalAreaRectangle = 0.00;
-        int numOfSurfaces=0;
+        double tempArea;
+        double totalAreaRectangle=0;
+        int numOfSurfaces;
         double height;
         double width;
         int currentSurface = 1;
 
-        System.out.println("How many Square / Rectangles would you like to enter measurements for?");
-        numOfSurfaces = sc.nextInt();
+        numOfSurfaces = validInputInt("How many Squares / Rectangles would you like to enter measurements for?");
 
         for(int i = 0; i < numOfSurfaces; i++ ){
-            System.out.println("Enter Height Dimension in Meters for surface: " + currentSurface);
-            height = sc.nextDouble();
-            System.out.println("Enter Width Dimension in Meters for surface " + currentSurface);
-            width = sc.nextDouble();
+            System.out.println("For Square/Rectangle number: " + currentSurface);
+            height = validInputDouble("Enter Height Dimension in Meters: ");
+            System.out.println("For Square/Rectangle number: " + currentSurface);
+            width = validInputDouble("Enter Width Dimension in Meters: ");
 
             tempArea = height*width;
             totalAreaRectangle = totalAreaRectangle + tempArea;
             currentSurface++;
-
         }
-        System.out.println(totalAreaRectangle);
-
+        //System.out.println(totalAreaRectangle);
         return totalAreaRectangle;
     }
     public static double surfaceAreaTriangle(){
-        Scanner sc = new Scanner(System.in);
-        double tempArea = 0.00;
+        double tempArea;
         double totalAreaTriangle = 0.00;
-        int numOfSurfaces=0;
+        int numOfSurfaces;
         double height;
         double base;
         int currentSurface = 1;
 
-        System.out.println("How many Triangles would you like to enter measurements for?");
-        numOfSurfaces = sc.nextInt();
+        numOfSurfaces = validInputInt("How many Triangles would you like to enter measurements for?");
 
         for(int i = 0; i < numOfSurfaces; i++ ){
-            System.out.println("Enter Height Dimension in Meters for surface: " + currentSurface);
-            height = sc.nextDouble();
-            System.out.println("Enter Base Dimension in Meters for surface " + currentSurface);
-            base = sc.nextDouble();
+            System.out.println("For Triangle number: " + currentSurface);
+            height = validInputDouble("Enter Height Dimension in Meters: ");
+            System.out.println("For Triangle number: " + currentSurface);
+            base = validInputDouble("Enter Base Dimension in Meters: ");
 
             tempArea = (height*base)/2;
             totalAreaTriangle = totalAreaTriangle + tempArea;
             currentSurface++;
-
         }
-        System.out.println(totalAreaTriangle);
-
+        //System.out.println(totalAreaTriangle);
         return totalAreaTriangle;
-
     }
     public static double surfaceAreaCircle(){
-        Scanner sc = new Scanner(System.in);
-        double tempArea = 0.00;
+        double tempArea;
         double totalAreaCircle = 0.00;
-        int numOfSurfaces=0;
+        int numOfSurfaces;
         double radius;
         int currentSurface = 1;
 
-        System.out.println("How many Triangles would you like to enter measurements for?");
-        numOfSurfaces = sc.nextInt();
+        numOfSurfaces = validInputInt("How many Circles would you like to enter measurements for?");
 
         for(int i = 0; i < numOfSurfaces; i++ ){
-            System.out.println("Enter Radius Dimension in Meters for surface: " + currentSurface);
-            radius = sc.nextDouble();
-
+            System.out.println("For Circle number: " + currentSurface);
+            radius = validInputDouble("Enter Radius Dimension in Meters: ");
             tempArea = Math.PI*(radius*radius);
             totalAreaCircle = totalAreaCircle + tempArea;
             currentSurface++;
-
         }
-        System.out.println(totalAreaCircle);
-
+        //System.out.println(totalAreaCircle);
         return totalAreaCircle;
     }
-    public static double surfaceAreaSemiCircle(){
-        Scanner sc = new Scanner(System.in);
-        double tempArea = 0.00;
-        double totalAreaSemiCircle = 0.00;
-        int numOfSurfaces=0;
-        double radius;
+    public static double surfaceAreaOval(){
+        double tempArea;
+        double totalAreaOval = 0.00;
+        int numOfSurfaces;
+        double radiusOne;
+        double radiusTwo;
         int currentSurface = 1;
 
-        System.out.println("How many Triangles would you like to enter measurements for?");
-        numOfSurfaces = sc.nextInt();
+        numOfSurfaces = validInputInt("How many Ovals would you like to enter measurements for?");
 
         for(int i = 0; i < numOfSurfaces; i++ ){
-            System.out.println("Enter Radius Dimension in Meters for surface: " + currentSurface);
-            radius = sc.nextDouble();
+            System.out.println("for Oval number: " + currentSurface);
+            radiusOne = validInputDouble("Enter the first Radius Dimension in Meters:");
+            System.out.println("For Oval number: " + currentSurface);
+            radiusTwo = validInputDouble("Enter the second Radius Dimension in Meters:");
 
-            tempArea = (Math.PI*(radius*radius))/2;
+            tempArea = Math.PI*(radiusOne*radiusTwo);
+            totalAreaOval = totalAreaOval + tempArea;
+            currentSurface++;
+        }
+        //System.out.println(totalAreaOval);
+        return totalAreaOval;
+    }
+    public static double surfaceAreaSemiCircle(){
+        double tempArea;
+        double totalAreaSemiCircle = 0.00;
+        int numOfSurfaces;
+        double radiusOne;
+        double radiusTwo;
+        int currentSurface = 1;
+
+        numOfSurfaces = validInputInt("How many Semi Circles would you like to enter measurements for?");
+
+        for(int i = 0; i < numOfSurfaces; i++ ){
+            System.out.println("for Semi-Circle number: " + currentSurface);
+            radiusOne = validInputDouble("Enter the first Radius Dimension in Meters:");
+            System.out.println("For Semi-Circle number: " + currentSurface);
+            radiusTwo = validInputDouble("Enter the second Radius Dimension in Meters:");
+
+            tempArea = (Math.PI*(radiusOne*radiusTwo))/2;
             totalAreaSemiCircle = totalAreaSemiCircle + tempArea;
             currentSurface++;
-
         }
-        System.out.println(totalAreaSemiCircle);
-
+        //System.out.println(totalAreaSemiCircle);
         return totalAreaSemiCircle;
     }
     public static double surfaceAreaTrapezoid(){
-        Scanner sc = new Scanner(System.in);
-        double tempArea = 0.00;
+        double tempArea;
         double totalAreaTrapezoid = 0.00;
-        int numOfSurfaces=0;
+        int numOfSurfaces;
         double height;
         double base;
         double base2;
         int currentSurface = 1;
 
-        System.out.println("How many Square / Rectangles would you like to enter measurements for?");
-        numOfSurfaces = sc.nextInt();
+        //System.out.println("How many Trapezoids would you like to enter measurements for?");
+        numOfSurfaces = validInputInt("How many Trapezoids would you like to enter measurements for?");
 
         for(int i = 0; i < numOfSurfaces; i++ ){
-            System.out.println("Enter Height Dimension in Meters for surface: " + currentSurface);
-            height = sc.nextDouble();
-            System.out.println("Enter the first base Dimension in Meters for surface " + currentSurface);
-            base = sc.nextDouble();
-            System.out.println("Enter the second base Dimension in Meters for surface " + currentSurface);
-            base2 = sc.nextDouble();
+            System.out.println("For Trapezoid number: " + currentSurface);
+            height = validInputDouble("Enter Height Dimension in Meters: ");
+            System.out.println("For Trapezoid number: " + currentSurface);
+            base = validInputDouble("Enter the first base Dimension in Meters: ");
+            System.out.println("For Trapezoid number: " + currentSurface);
+            base2 = validInputDouble("Enter the second base Dimension in Meters: ");
 
             tempArea = ((base*base2)/2)*height;
             totalAreaTrapezoid = totalAreaTrapezoid + tempArea;
             currentSurface++;
-
         }
         System.out.println(totalAreaTrapezoid);
 
         return totalAreaTrapezoid;
     }
     public static double coveragePerLitreCalc(){
-        double result= 0.00;
-        double squareMeter = 0.00;
-        double liter = 0.00;
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Please enter the coverage of your paint per square meter");
-        System.out.print("Please input the square meters: ");
-        squareMeter = sc.nextDouble();
-        System.out.print("Please input the liters: ");
-        liter = sc.nextDouble();
+        double result;
+        double squareMeter;
+        double liter;
+        squareMeter = validInputDouble("Please enter the coverage of your paint per square meter\n" + "Please input the square meters: ");
+        liter = validInputDouble("Please input the liters: ");
         result = squareMeter/liter;
 
         return result;
     }
     public static double question(){
-        Scanner scanner = new Scanner(System.in);
         double surfaceArea = 0.00;
 
-        System.out.println("Please select how many surface types you wish to calculate for (Square/Rectangles are considered as the same)");
-        System.out.println("E.g. 3 (Circle, Triangle, Square) (as an integer e.g. 1,2,3,4, etc)");
-        int numOfSurfaceTypes = scanner.nextInt();
+        //int numOfSurfaceTypes = validInputInt("Please select how many surface types you wish to calculate for (Square/Rectangles are considered as the same)\n" + " E.g. 3 (Circle, Triangle, Square) (as an integer e.g. 1,2,3,4, etc)");
+        int numOfSurfaceTypes = validInputInt("""
+                Please select how many different surface types you wish to calculate for
+                E.g. 3 (Circle, Triangle, Square) (as an integer e.g. 1,2,3,4, etc)
+                (Square/Rectangles are considered as the same)""");
 
         do {
-            System.out.println("Please select which surface type you would like to calculate for");
-            System.out.println("1: Rectangle / Square");
-            System.out.println("2: Triangle");
-            System.out.println("3: Circle");
-            System.out.println("4: Semi-Circle");
-            System.out.println("5: Trapezoid");
-
-            int userChoiceSurface = scanner.nextInt();
+            int userChoiceSurface = validInputInt("""
+                    Please select which surface type you would like to calculate for
+                    1: Rectangle / Square
+                    2: Triangle
+                    3: Circle
+                    4: Semi-Circle
+                    5: Trapezoid
+                    6: Oval""");
 
             switch (userChoiceSurface){
                 case 1:
@@ -193,65 +192,102 @@ public class Main {
                     System.out.println("5: Trapezoid");
                     surfaceArea = surfaceArea + surfaceAreaTrapezoid();
                     break;
+                case 6:
+                    System.out.println("5: Oval");
+                    surfaceArea = surfaceArea + surfaceAreaOval();
+                    break;
                 default:
                     System.out.println("Not valid input");
-
                     break;
             }
-
             numOfSurfaceTypes--;
         }
         while(numOfSurfaceTypes != 0);
         return surfaceArea;
     }
-    public static void error(){
-
+    public static double validInputDouble(String question){
+        Scanner sc = new Scanner(System.in);
+        boolean valid= false;
+        double userResponse = 0.00;
+        do {
+            System.out.println(question);
+            if(sc.hasNextDouble()) {
+                double tempUserResponse = sc.nextDouble();
+                if(tempUserResponse>0.00){
+                    userResponse = tempUserResponse;
+                    valid = true;
+                }
+                else{
+                    System.out.println("Please input only positive numbers greater than 0");
+                }
+            }
+            else {
+                System.out.println("Please enter numbers in numerical format");
+                sc.next();
+            }
+        }while(!valid);
+        return userResponse;
+    }
+    public static int validInputInt (String question){
+        Scanner sc = new Scanner(System.in);
+        boolean valid= false;
+        int userResponse = 0;
+        do {
+            System.out.println(question);
+            if(sc.hasNextInt()) {
+                int tempUserResponse = sc.nextInt();
+                if(tempUserResponse>0){
+                    userResponse = tempUserResponse;
+                    valid = true;
+                }
+                else{
+                    System.out.println("Please input only positive numbers ");
+                }
+            }
+            else {
+                System.out.println("Please whole numbers in numerical format");
+                sc.next();
+            }
+        }while(!valid);
+        return userResponse;
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double surfaceArea = 0.00;
-        double coveragePerLitre = 0.00;
-        double volumeNeeded = 0.00;
+        double surfaceArea;
+        double coveragePerLitre;
+        double volumeNeeded;
         double removableSurfaceArea = 0.00;
-        double finalSurfaceArea = 0.00;
+        double finalSurfaceArea;
+        boolean validInput = true;
 
         surfaceArea = question();
-
         System.out.println(surfaceArea);
 
-        System.out.println("Do you have any non-paint surfaces on the surfaces you mentioned earlier? (1 for yes or 2 for no)");
-        int response = scanner.nextInt();
-        if (response == 1){
-            System.out.println("you have outletssss");
-            removableSurfaceArea = question();
-
-        } else if (response == 2) {
-            System.out.println("you have NOOO outlets");
-            removableSurfaceArea = 0.00;
-        }
-        else {
-            System.out.println("Not valid choice");
-        }
+        do {
+            int nonPaintResponse = validInputInt("Do you have any non-paint surfaces on the surfaces you mentioned earlier? (1 for yes or 2 for no)");
+            if (nonPaintResponse==1 || nonPaintResponse== 2){
+                if (nonPaintResponse == 1){
+                    removableSurfaceArea = question();
+                } else{
+                    removableSurfaceArea = 0.00;
+                }
+            } else {
+                System.out.println("Not valid choice");
+                validInput = false;
+            }
+        }while(!validInput);
 
         coveragePerLitre = coveragePerLitreCalc();
-
         finalSurfaceArea = surfaceArea - removableSurfaceArea;
 
-        System.out.println("How many coats of paint are required?");
-        int numOfCoats = scanner.nextInt();
-
+        int numOfCoats = validInputInt("How many coats of paint are required?");
         volumeNeeded = (finalSurfaceArea / coveragePerLitre) * numOfCoats;
 
+        double litrePerCan = validInputDouble("How many Litres of paint are in one paint-can");
+        double numOfCansNeeded = (volumeNeeded/litrePerCan);
+        double finalCansNeeded = Math.ceil(numOfCansNeeded);
+
         System.out.println("You need: " + volumeNeeded + " Litres of paint");
-
-        System.out.println("How many Litres of paint are in one paint-can");
-        double litrePerCan = scanner.nextDouble();
-
-        int numOfCansNeeded = (int) (volumeNeeded/litrePerCan);
-
-        System.out.println("You need " + " number of " + litrePerCan + "L cans");
-
+        System.out.println("You need " + finalCansNeeded + " number of " + litrePerCan + "L cans");
     }
-
 }
